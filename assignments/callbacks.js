8f18e31ee69257f95eec9394ef8b956edb8149d7
Nurudeen <nurudeen.amedu@gmail.com>
2019-08-20 11:44:29 +0100

    callbacks tasks completed

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,4 +1,8 @@
-// Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
+// Create a higher order function and invoke the callback function to test 
+// your work. You have been provided an example of a problem and a solution 
+// to see how this works with our items array.  
+// Study both the problem and the solution to figure out the rest of the 
+// problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
@@ -37,33 +41,60 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   firstItem(items, logExorbitantPrice);
   // "this Pencil is worth a million dollars!"
 */
-
+function logText(quote){
+  console.log(quote);
+}
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, logText);
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length-1]);
 }
+
+last(items, logText);
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x+y);
 }
+
+sumNums(3, 4, logText);
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x*y);
 }
+
+multiplyNums(3, 4, logText);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  let foundItem = list.filter(value => value.toLowerCase() === item.toLowerCase());
+  if(foundItem.length > 0){
+    return cb(true);
+  }else{
+    return cb(false);
+  }
 }
 
+contains('pencil', items, logText);
+
 /* STRETCH PROBLEM */
+
+const items2 = ['Pencil', 'yo-yo', 'Notebook', 'Notebook', 'yo-yo', 'Gum'];
 
 function removeDuplicates(array, cb) {
   // removeDuplicates removes all duplicate values from the given array.
   // Pass the duplicate free array to the callback function.
   // Do not mutate the original array.
+  return cb(array.filter((item, index) => array.indexOf(item)===index));
 }
+
+removeDuplicates(items2, logText);
